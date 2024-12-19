06b10091f93bf7c647bf660133242ebdf26b3323
Shreyansh Shukla <shreyansh.14010@gmail.com>
2024-12-20 00:50:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return <Button>Hello ShadCN</Button>
+}
+
+export default App;
